f508f3b5518573c92ee1e025d3fe783b51c7f20a
sushantpokhrel <sushant.805421@cct.tu.edu.np>
2026-08-12 18:42:25 +0545

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl text-cyan-400 text-center m-20'>MESSIII</h1>
+    </>
+  )
+}
+
+export default App
